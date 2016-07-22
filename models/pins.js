fe1ddf629cb241049f5f0ad227b4a432a48aa55f
Ryan Brenner <ryanbrenner@Ryans-MacBook-Air.local>
2016-07-21 18:27:37 -0700

    needed to find a quieter place to work, commiting just incase, started pins model

---
+++ models/pins.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose'),
+var Schema = mongoose.Schema;
+
+var PinSchema = new Schema ({
+ 
+
+locationName: String
+coordinates:{(ongitude: Number , latitude: Number},
+date_created: Date
+time created: (type:Date, default: newTime() ),
+averagerating: Number 
+share: Boolean
+groupId:
