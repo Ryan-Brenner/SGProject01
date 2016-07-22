44c69bcd62d32dc236f662ce6d548c000e1c343d
Ryan Brenner <ryanbrenner@Ryans-MacBook-Air.local>
2016-07-21 17:49:51 -0700

    mental break: updated colors, have sign in icon and image on homepage has a backgroud/border

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,9 @@
 console.log("Sanity Check: JS is working!");
 $(document).ready(function() {
 
-
 var apod_endpoint = "https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo";
+
+
 
 
 $.get(apod_endpoint).success(function handleApodSuccess(apod){
@@ -15,12 +16,12 @@ $.get(apod_endpoint).success(function handleApodSuccess(apod){
   }
   
   if(apod.media_type == "video") {
-    $("#apod_img_id").css("display", "none"); 
-    $("#apod_vid_id").attr("src", apod.url);
+    $("#apod_img").css("display", "none"); 
+    $("#apod_vid").attr("src", apod.url);
   }
   else {
-    $("#apod_vid_id").css("display", "none"); 
-    $("#apod_img_id").attr("src", apod.url);
+    $("#apod_vid").css("display", "none"); 
+    $("#apod_img").attr("src", apod.url);
   }
   $("#reqObject").text(apod_endpoint);
   $("#returnObject").text(JSON.stringify(apod, null, 4));  
